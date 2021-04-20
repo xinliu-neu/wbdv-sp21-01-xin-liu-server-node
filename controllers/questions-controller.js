666050888383e94b88d9be1073d898c6ed0a6191
Xin Liu <liu.xin7@northeastern.edu>
2021-04-19 20:49:23 -0700

    Refactored controllers

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -1,26 +1,57 @@
-const questionService = require("../services/questions/questions-service")
+// const questionService = require("../services/questions/questions-service")
+//
+// module.exports = (app) => {
+//
+//   const findAllQuestions = (req, res) => {
+//     // const questions = questionService.findAllQuestions();
+//     // res.send(questions)
+//     questionService.findAllQuestions()
+//       .then((questions) => {
+//         res.send(questions)
+//       })
+//   }
+//
+//   const findQuestionsForQuiz = (req, res) => {
+//     const quizId = req.params.qzid;
+//     // const questions = questionService.findQuestionsForQuiz(quizId);
+//     // res.send(questions);
+//     questionService.findQuestionsForQuiz(quizId)
+//     .then((questions) => {
+//       res.send(questions)
+//     })
+//
+//
+//     questionService.findQuestionsForQuiz(quizId)
+//       .then((questions) => {
+//         res.send(questions)
+//       })
+//
+//   }
+//
+//   const findQuestionById = (req, res) => {
+//     const quid = req.params['questionId']
+//     const question = questionService.findQuestionById(quid);
+//     res.send(question);
+//   }
+//
+//   app.get("/api/questions", findAllQuestions);
+//   app.get("/api/quizzes/:qzid/questions", findQuestionsForQuiz);
+//   app.get("/api/quizzes/:quizId/:questionId", findQuestionById);
+// }
+//
 
-module.exports = (app) => {
 
-  const findAllQuestions = (req, res) => {
-    const questions = questionService.findAllQuestions();
-    res.send(questions)
-  }
+const questionsService = require('../services/questions/questions-service')
+module.exports = function(app) {
+  app.get('/api/quizzes/:qid/questions', (req, res) =>
+      questionsService.findQuestionsForQuiz(req.params['qid'])
+      .then(questions => res.json(questions)))
 
-  const findQuestionsForQuiz = (req, res) => {
-    const quizId = req.params.qzid;
-    const questions = questionService.findQuestionsForQuiz(quizId);
-    res.send(questions);
-  }
-
-  const findQuestionById = (req, res) => {
-    const quid = req.params['questionId']
-    const question = questionService.findQuestionById(quid);
-    res.send(question);
-  }
-
-  app.get("/api/questions", findAllQuestions);
-  app.get("/api/quizzes/:qzid/questions", findQuestionsForQuiz);
-  app.get("/api/quizzes/:quizId/:questionId", findQuestionById);
+  app.get('/api/questions', (req, res) =>
+      questionsService.findAllQuestions()
+      .then(allQuestions => res.json(allQuestions)))
+  app.get('/api/questions/:qid', (req, res) =>
+      questionsService.findQuestionById(req.params['qid'])
+      .then(question => res.json(question)))
 }
 
--- controllers/quizzes-controller.js
+++ controllers/quizzes-controller.js
@@ -1,17 +1,37 @@
+// const quizzesService = require('../services/quizzes/quiz-service')
+//
+// module.exports = (app) => {
+//   const findAllQuizzes = (req, res) => {
+//     // const quizzes = quizzesService.findAllQuizzes()
+//     // res.send(quizzes)
+//     quizzesService.findAllQuizzes()
+//         .then((quizzes) => {
+//           res.send(quizzes)
+//         })
+//   }
+//
+//   const findQuizById = (req, res) => {
+//     const quizId = req.params['qid']
+//     // const quiz = quizzesService.findQuizById(quizId)
+//     // res.send(quiz)
+//     quizzesService.findQuizById(quizId)
+//         .then((quiz) => {
+//           res.send(quiz)
+//     })
+//   }
+//
+//   app.get("/api/quizzes", findAllQuizzes)
+//   app.get("/api/quizzes/:qid", findQuizById)
+// }
+
+
 const quizzesService = require('../services/quizzes/quiz-service')
-
-module.exports = (app) => {
-  const findAllQuizzes = (req, res) => {
-    const quizzes = quizzesService.findAllQuizzes()
-    res.send(quizzes)
-  }
-
-  const findQuizById = (req, res) => {
-    const quizId = req.params['qid']
-    const quiz = quizzesService.findQuizById(quizId)
-    res.send(quiz)
-  }
-
-  app.get("/api/quizzes", findAllQuizzes)
-  app.get("/api/quizzes/:qid", findQuizById)
+module.exports = function (app) {
+  app.get('/api/quizzes', (req, res) =>
+      quizzesService.findAllQuizzes()
+      .then(allQuizzes => res.json(allQuizzes)))
+  app.get('/api/quizzes/:qzid', (req, res) =>
+      quizzesService.findQuizById(req.params['qzid'])
+      .then(quiz => res.json(quiz)))
 }
+
