ca2f310d0b851fd355b3465cecbb04a30160a270
Xin Liu <liu.xin7@northeastern.edu>
2021-04-19 20:49:44 -0700

    Refactored services

--- services/questions/questions-service.js
+++ services/questions/questions-service.js
@@ -1,31 +1,42 @@
-const questions = require('./questions.json')
+// const questions = require('./questions.json')
+// const questionsModel = require("../../models/questions/question-model")
+// const createQuestion = () => {}
+// const createQuestionForQuiz = () => {}
+//
+// const findAllQuestions = () => {
+//   return questionsModel.find()
+//   // return questions;
+// }
+//
+// const findQuestionsForQuiz = (qzid) => {
+//   // return questions.filter(question => question.quizId === qzid);
+//   return questionsModel.find({quizId: "345"})
+// }
+//
+//
+// const findQuestionById = (quid) => {
+//   // return questions.find(question => question._id === quid);
+//   return questionsModel.findById(quid)
+// }
+// const updateQuestion = () => {}
+// const deleteQuestion = () => {}
+//
+//
+//
+// module.exports = {
+//   createQuestion,
+//   createQuestionForQuiz,
+//   findQuestionsForQuiz,
+//   findAllQuestions,
+//   findQuestionById,
+//   updateQuestion,
+//   deleteQuestion,
+// }
 
-const createQuestion = () => {}
-const createQuestionForQuiz = () => {}
 
-const findAllQuestions = () => {
-  return questions;
-}
+const questionsDao = require('../../daos/questions-dao')
+const findAllQuestions = () => questionsDao.findAllQuestions()
+const findQuestionById = (qid) => questionsDao.findQuestionById(qid)
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid)
+module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
 
-const findQuestionsForQuiz = (qzid) => {
-  return questions.filter(question => question.quizId === qzid);
-}
-
-
-const findQuestionById = (quid) => {
-  return questions.find(question => question._id === quid);
-}
-const updateQuestion = () => {}
-const deleteQuestion = () => {}
-
-
-
-module.exports = {
-  createQuestion,
-  createQuestionForQuiz,
-  findQuestionsForQuiz,
-  findAllQuestions,
-  findQuestionById,
-  updateQuestion,
-  deleteQuestion,
-}
