55ba8c90f94fbee402117d9bb52521dd006aa110
Xin Liu <liu.xin7@northeastern.edu>
2021-04-19 20:49:58 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,22 @@
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/whiteboard',
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+// const session = require('express-session')
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   // cookie: { secure: true }
+// }))
 
 // configure CORS
 app.use(function (req, res, next) {
@@ -12,9 +28,13 @@ app.use(function (req, res, next) {
       'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 });
+//
+// require("./controllers/quizzes-controller")(app)
+// require("./controllers/questions-controller")(app)
 
-require("./controllers/quizzes-controller")(app)
-require("./controllers/questions-controller")(app)
+require('./controllers/quizzes-controller')(app)
+require('./controllers/questions-controller')(app)
+require('./controllers/quiz-attempts-controller')(app)
 
 app.listen(3000)
 
